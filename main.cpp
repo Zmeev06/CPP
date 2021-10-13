3f907542939e4fd1daaa8f51d9cfe6c754f720d8
Zmeev06 <se.rg.ey06@icloud.com>
2021-10-13 23:16:09 +0300

    a

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    int y;
+    cin >> x;
+    cin >> y;
+    cout << sqrt(abs(pow(x,y)-cos(y))) << endl;
+
+    
+}
